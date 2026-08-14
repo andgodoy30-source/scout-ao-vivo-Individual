222df34c68eee66c352bae1366b0ca578edee9d1
andgodoy30-source <andgodoy30@gmail.com>
2026-08-14 10:25:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='scout-individual-beta4-v1',A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const C='scout-individual-beta5-v1',A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))));
